bd264bfbb343bd5ef172faf5d21e53bc0e01e2a0
koriblicek <koriblik@gmail.com>
2023-11-23 10:27:36 +0100

    project name corrected

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,47 @@
-import React from 'react';
+import { Provider } from 'react-redux';
+import { store } from './store/store';
+import { IAppInputData } from './types';
+import { Alert, AlertTitle } from '@mui/material';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const rootElement = document.getElementById('advertisement-root') as HTMLElement;
+const root = ReactDOM.createRoot(rootElement);
+
+//input data
+let inputData: IAppInputData | undefined;
+
+//error 
+let errorStatus = false;
+let errorMessage = "";
+
+if (root) {
+  const dal = "https://api.ralph.sk/apijs/";
+  const di = rootElement.getAttribute("data-id");
+  const dm = rootElement.getAttribute("data-module");
+  if ((dal !== null) && (di !== null) && (dm !== null)) {
+    inputData = {
+      dataApiLink: "https://api.ralph.sk/apijs/",
+      dataId: di,
+      dataModule: dm
+    };
+  } else {
+    errorStatus = true;
+    errorMessage = "Required input is missing!";
+  }
+} else {
+  errorStatus = true;
+  errorMessage = "Root node '<div id=\"debuilder-root\"></div>' not found!";
+}
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // <React.StrictMode>
+  <Provider store={store}>
+    {inputData && <App inputData={inputData} />}
+    {errorStatus && <Alert variant='standard' severity='error' sx={{ m: 1 }} >
+      <AlertTitle>Data error</AlertTitle>
+      {errorMessage}</Alert>}
+  </Provider>
+  // </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
